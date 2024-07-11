49e69f1d1692cc2ab64499b4b8103a742d666fff
rysterman34 <ryanpapetti@gmail.com>
2024-07-11 18:54:43 +0200

    Return success statement with API

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 #Logging initialization
@@ -6,6 +5,11 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+def gather_artists_names(artists):
+    return '& '.join([artist['name'] for artist in artists])
+
+def listening_statement(artist_str, track_str):
+    return f"Ryan is listening to {track_str} by {artist_str}"
 
 def gather_current_listening_data(headers):
     session = requests.session()
@@ -17,23 +21,18 @@ def gather_current_listening_data(headers):
     if current_playing_response.status_code != 204:
         response_data = current_playing_response.json()
 
-        artist_name = response_data['item']['artists'][0]['name']
+        artists_names = gather_artists_names(response_data['item']['artists'])
         track_name = response_data['item']['name']
-        relevant_album_images = response_data['item']['album']['images']
-        eventual_body = {'track_name':track_name,'artist_name':artist_name, 'images':relevant_album_images}
 
-        # payload_returned = {'statusCode': 200, 'headers': {'Access-Control-Allow-Origin' : "*", 'content-type': 'application/json'}, 'body': json.dumps(response_data), "isBase64Encoded": "false"}
+        eventual_body = {'statement': listening_statement(artists_names, track_name)}
 
         default_payload['body'] = eventual_body
-
-
 
         logger.info(f"{default_payload}")
         return default_payload
     
     else:
         default_payload['body'] = {'statement':'Ryan is not listening to anything on Spotify right now.'}
-        # payload_returned = {'statusCode': 200, 'headers': {'Access-Control-Allow-Origin' : "*", 'content-type': 'application/json'}, 'body': json.dumps({'music':'Ryan is not playing any music right now'}), "isBase64Encoded": "false"}
         logger.info('Not listening right now')
         return default_payload
 
